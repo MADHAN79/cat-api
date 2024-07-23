1a36afb76f5732d8277339cac1d3f4a01f3ce450
MADHAN K <73920531+MADHAN79@users.noreply.github.com>
2024-07-23 19:42:07 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const catQuoteElement = document.getElementById("catQuote");
+    const generateBtn = document.getElementById("generateBtn");
+
+    function fetchCatQuote() {
+        fetch('https://catfact.ninja/fact')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('catQuote').innerText = data.fact;
+            })
+            .catch(error => {
+                catQuoteElement.innerText = "Failed to fetch a new quote.";
+                console.error(error);
+            });
+    }
+
+    function runConfetti() {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
+        });
+    }
+
+    generateBtn.addEventListener("click", function() {
+        fetchCatQuote();
+        runConfetti();
+    });
+
+    fetchCatQuote();
+});
